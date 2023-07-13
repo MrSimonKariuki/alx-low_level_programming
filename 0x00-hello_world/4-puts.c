63e413fafbcb7f7721137d13cd43cdba752f0875
MrSimonKariuki <simonsoftengineer@gmail.com>
2023-07-13 10:31:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A functon that prints a line with puts function
+ *Return:0(Success)
+ */
+int main(void)
+{
+puts("\"Progmming is like building a multilingual puzzle");
+return (0);
+}
+
