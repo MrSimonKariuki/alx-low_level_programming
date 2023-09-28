631323cba73452f69e5180d86a74bff703bd515d
mrsimonkariuki <simonsoftengineer@gmail.com>
2023-09-28 15:31:00 -0700

    update2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,22 @@
-/*
- * This program defines a function that counts the number of bits that need
- * to be changed in order to transform one unsigned long integer into another.
- * It calculates the exclusive OR (XOR) of the two numbers and counts the
- * number of set bits in the result.
- */
 #include "main.h"
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 int i, count = 0;
 unsigned long int current;
 unsigned long int exclusive = n ^ m;
-
 for (i = 63; i >= 0; i--)
 {
 current = exclusive >> i;
 if (current & 1)
 count++;
 }
-
 return (count);
 }
